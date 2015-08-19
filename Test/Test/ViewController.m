e1ce126c96a6f4b5cc79cba095cb14b3d1f7f07d
Swiffft <tarena@heheda.local>
2015-08-19 14:50:07 +0800

    添加属性

--- Test/Test/ViewController.m
+++ Test/Test/ViewController.m
@@ -9,13 +9,17 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong)NSString *name;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSArray *a = @[@"1",@"2",@"3"];
+    for (NSInteger i = a.count-1; i >=0 ; i--) {
+        NSLog(@"%@",a[i]);
+    }
     // Do any additional setup after loading the view, typically from a nib.
 }
 
